12998d88a58c01e745fbacfa711315a68977d88a
최정필 <wjdvlf1407@gmail.com>
2022-05-08 21:52:43 +0900

    맥북 실험중

--- src/echo01/Client.java
+++ src/echo01/Client.java
@@ -14,55 +14,62 @@ import java.util.Scanner;
 public class Client {
 
 	public static void main(String[] args) throws IOException {
-
-		Socket socket = new Socket();
-
-		System.out.println("<클라이언트 시작>");
-		System.out.println("==========================================");
-
-		System.out.println("[서버에 연결을 요청합니다.]");
-		socket.connect(new InetSocketAddress("127.0.0.1", 8080));
 		
-
+		Socket socket = new Socket();
+		
+		System.out.println("<클라이언트 시작>");
+		System.out.println("============================");
+		
+		System.out.println("[서버에 연결을 요청합니다]");
+		socket.connect(new InetSocketAddress("192.168.0.55", 10001));
+		
 		System.out.println("[서버에 연결 되었습니다.]");
-
+		
 		// 메세지 보내기용 스트림
-		OutputStream os = socket.getOutputStream();
+		OutputStream os = socket.getOutputStream();	// new 불필요, 알아서 세팅되어있음
 		OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
 		BufferedWriter bw = new BufferedWriter(osw);
-
+		
 		// 메세지 받기용 스트림
 		InputStream is = socket.getInputStream();
 		InputStreamReader isr = new InputStreamReader(is, "UTF-8");
 		BufferedReader br = new BufferedReader(isr);
-
+		
+		
 		// 스캐너
 		Scanner sc = new Scanner(System.in);
-
-		while (true) {
-			// 키보드입력
+		
+		// 반복문 적용
+		while(true) {
+			// 키보드 입력값
 			String str = sc.nextLine();
-
-			if (str.equals("/q")) {
+			
+			if(str.equals("/q")) {
 				break;
 			}
+			
 			// 메세지 보내기
 			bw.write(str);
+			// 현재 정보량이 매우 적어 전송이 되지 않음(손님을 더 기다리는 중)
 			bw.newLine();
-			bw.flush();
-
+			bw.flush();	// 강제 송출
+			
 			// 메세지 받기
 			String reMsg = br.readLine();
-			System.out.println("server:[" + reMsg + "]");
+			System.out.println("server: [" + reMsg + "]");
+			
 		}
-
-		System.out.println("=======================================");
+		
+		System.out.println("=============================");
 		System.out.println("<클라이언트 종료>");
-
+		
 		sc.close();
+		br.close();
 		bw.close();
 		socket.close();
-
+		
+		
+		
 	}
 
 }
--- src/echo01/Server.java
+++ src/echo01/Server.java
@@ -14,48 +14,51 @@ import java.net.Socket;
 public class Server {
 
 	public static void main(String[] args) throws IOException {
+		// TCP 소켓 프로그래밍 - 서버 소켓
 		ServerSocket serverSocket = new ServerSocket();
-		serverSocket.bind(new InetSocketAddress("127.0.0.1", 8080)); // ip는 문자열
-
+		serverSocket.bind(new InetSocketAddress("192.168.0.55", 10001)); // (ip주소, port)
+		
 		System.out.println("<서버 시작>");
-		System.out.println("==================================================");
+		System.out.println("============================================================");
 		System.out.println("[연결을 기다리고 있습니다.]");
-
-		Socket socket = serverSocket.accept();
-		System.out.println("[클라이언트가 연결 되었습니다.]");
-
+		
+		Socket socket = serverSocket.accept();	// 소켓 객체 생성 command
+		System.out.println("[클라이언트가 연결되었습니다]");
+		
 		// 메세지 받기용 스트림
 		InputStream is = socket.getInputStream();
 		InputStreamReader isr = new InputStreamReader(is, "UTF-8");
 		BufferedReader br = new BufferedReader(isr);
-
-		// 메세지 보내기용 스트링
+		
+		// 메세지 보내기용 스트림
 		OutputStream os = socket.getOutputStream();
 		OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
 		BufferedWriter bw = new BufferedWriter(osw);
-
-		while (true) {
+		
+		// 클라이언트 while에 대응하여 반복 수령 필요함
+		while(true) {
+			// 메세지 받기
 			String msg = br.readLine();
-
-			if (msg == null) {
-				System.out.println("");
+			
+			if(msg==null) {
 				break;
 			}
-
-			System.out.println("받은메세지: " + msg);
-
+			
+			System.out.println("받은메세지:" + msg);
 			// 메세지 보내기
 			bw.write(msg);
 			bw.newLine();
 			bw.flush();
 		}
-
-		System.out.println("==================================================");
+		
+		
+		System.out.println("============================================================");
 		System.out.println("<서버 종료>");
-
-		br.close();
 		bw.close();
+		br.close();
 		socket.close();
 		serverSocket.close();
+		
 	}
+
 }
