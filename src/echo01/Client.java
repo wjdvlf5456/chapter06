9e8204380d22f0950f466a1c1978da3387a8b136
최정필 <wjdvlf1407@gmail.com>
2022-05-09 09:53:31 +0900

    http400 bad

--- src/echo01/Client.java
+++ src/echo01/Client.java
@@ -7,7 +7,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.net.Inet4Address;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.util.Scanner;
@@ -19,15 +18,15 @@ public class Client {
 		Socket socket = new Socket();
 
 		System.out.println("<클라이언트 시작>");
-		System.out.println("============================");
+		System.out.println("==========================================");
 
-		System.out.println("[서버에 연결을 요청합니다]");
-		socket.connect(new InetSocketAddress(Inet4Address.getLoopbackAddress(), 8080));
+		System.out.println("[서버에 연결을 요청합니다.]");
+		socket.connect(new InetSocketAddress("192.168.0.55", 80));
 
 		System.out.println("[서버에 연결 되었습니다.]");
 
 		// 메세지 보내기용 스트림
-		OutputStream os = socket.getOutputStream();	// new 불필요, 알아서 세팅되어있음
+		OutputStream os = socket.getOutputStream();
 		OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
 		BufferedWriter bw = new BufferedWriter(osw);
 
@@ -39,32 +38,27 @@ public class Client {
 		// 스캐너
 		Scanner sc = new Scanner(System.in);
 
-		// 반복문 적용
 		while (true) {
-			// 키보드 입력값
+			// 키보드입력
 			String str = sc.nextLine();
 
 			if (str.equals("/q")) {
 				break;
 			}
-
 			// 메세지 보내기
 			bw.write(str);
-			// 현재 정보량이 매우 적어 전송이 되지 않음(손님을 더 기다리는 중)
 			bw.newLine();
-			bw.flush();	// 강제 송출
+			bw.flush();
 
 			// 메세지 받기
 			String reMsg = br.readLine();
-			System.out.println("server: [" + reMsg + "]");
-
+			System.out.println("server:[" + reMsg + "]");
 		}
 
-		System.out.println("=============================");
+		System.out.println("=======================================");
 		System.out.println("<클라이언트 종료>");
 
 		sc.close();
-		br.close();
 		bw.close();
 		socket.close();
 
